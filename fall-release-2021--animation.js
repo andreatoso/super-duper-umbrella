a1f8356c36db2a0ef1b32e17ae923624f224916b
Andrea Toso <andrea.toso99@gmail.com>
2021-11-01 11:07:40 +0100

    Update fall-release-2021--animation.js

--- fall-release-2021--animation.js
+++ fall-release-2021--animation.js
@@ -519,8 +519,4 @@ class Gradient {
 var gradient = new Gradient();
     gradient.initGradient("#gradient-canvas");
 var gradient2 = new Gradient();
-    gradient2.initGradient("#session-day-1");
-var gradient3 = new Gradient();
-    gradient3.initGradient("#session-day-2");
-var gradient4 = new Gradient();
-    gradient4.initGradient("#session-day-3");
+    gradient2.initGradient("#register-canvas");
